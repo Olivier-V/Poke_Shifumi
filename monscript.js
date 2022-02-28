37f1fc9b314f4e1db6e64f788013779ef1486301
Olivier Formateur <oliviervanni@aol.com>
2022-02-28 16:41:24 +0100

    mise en forme

--- monscript.js
+++ monscript.js
@@ -1,66 +1,64 @@
 var vous;
 var ordi;
-var vosPoints=0;
-var pointsOrdi=0;
+var vosPoints = 0;
+var pointsOrdi = 0;
 
 $("#vosPoints").text(vosPoints);
 $("#pointsOrdi").text(pointsOrdi);
 
-function Votrechoix(v){
-    vous=v;
-    setTimeout(Choixordi , 2000);
+function Votrechoix(v) {
+    vous = v;
+    setTimeout(Choixordi, 2000);
 }
 
-function Choixordi(){
-    ordi=Math.round(Math.random()*2)+1;
-    if (ordi == 1){
+function Choixordi() {
+    ordi = Math.round(Math.random() * 2) + 1;
+    if (ordi == 1) {
         $("#choixOrdi1").show();
         $("#choixOrdi2").hide();
         $("#choixOrdi3").hide();
-    }
-    else if ( ordi == 2){
+    } else if (ordi == 2) {
         $("#choixOrdi1").hide();
         $("#choixOrdi2").show();
         $("#choixOrdi3").hide();
-    }
-    else if ( ordi == 3){
+    } else if (ordi == 3) {
         $("#choixOrdi1").hide();
         $("#choixOrdi2").hide();
         $("#choixOrdi3").show();
     }
     Pipaci();
-    }
-    
-    function Pipaci(){
-    if((vous==1) && (ordi==3) || (vous==2) && (ordi==1) || (vous==3) && (ordi==2)){
+}
+
+function Pipaci() {
+    if ((vous == 1) && (ordi == 3) || (vous == 2) && (ordi == 1) || (vous == 3) && (ordi == 2)) {
         vosPoints += 10;
         $("#vosPoints").text(vosPoints);
         $("#pointsOrdi").text(pointsOrdi);
         $("#gagne").show()
-        if(vosPoints > 120 || pointsOrdi >120){
+        if (vosPoints > 120 || pointsOrdi > 120) {
             redirection()
         }
-    }else{
+    } else {
         Ordipipaci();
     }
 }
 
-function Ordipipaci(){
-    if((ordi==1) && (vous==3) || (ordi==2) && (vous==1) || (ordi==3) && (vous==2)){
+function Ordipipaci() {
+    if ((ordi == 1) && (vous == 3) || (ordi == 2) && (vous == 1) || (ordi == 3) && (vous == 2)) {
         pointsOrdi += 10;
         $("#vosPoints").text(vosPoints);
         $("#pointsOrdi").text(pointsOrdi);
         $("#perd").show();
-        if(vosPoints > 120 || pointsOrdi >120){
+        if (vosPoints > 120 || pointsOrdi > 120) {
             redirection()
         }
-    }else{
+    } else {
         vosPoints += 5;
         pointsOrdi += 5;
         $("#vosPoints").text(vosPoints);
-        $("#pointsOrdi").text(pointsOrdi);      
+        $("#pointsOrdi").text(pointsOrdi);
         $("#egal").show();
-        if(vosPoints > 120 || pointsOrdi >120){
+        if (vosPoints > 120 || pointsOrdi > 120) {
             redirection()
         }
     }
@@ -87,9 +85,9 @@ function masquerbubli() {
     audio.play();
 }
 
-function redirection(){
-    sessionStorage.setItem('vosPoints' , vosPoints);
-    sessionStorage.setItem('pointsOrdi' , pointsOrdi);
+function redirection() {
+    sessionStorage.setItem('vosPoints', vosPoints);
+    sessionStorage.setItem('pointsOrdi', pointsOrdi);
     window.location.replace("./gameover.html");
 }
 
@@ -144,8 +142,3 @@ function init() {
     $('#cara').slideUp(1000);
     $('#sala').slideUp(1000);
 }
-
-
-
-
-
